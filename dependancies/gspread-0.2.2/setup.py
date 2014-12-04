255f20747eba31cb8df04b91d92dfc93060802b6
gcool-info <george.koulouris1@gmail.com>
2014-12-04 18:29:44 -0500

    Restructuring code

---
+++ dependancies/gspread-0.2.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+
+import gspread
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+description = 'Google Spreadsheets Python API'
+
+long_description = """
+{index}
+
+License
+-------
+MIT
+
+Download
+========
+"""
+
+long_description = long_description.lstrip("\n").format(index=read('docs/index.txt'))
+
+setup(
+    name='gspread',
+    packages=['gspread'],
+    description=description,
+    long_description=long_description,
+    version=gspread.__version__,
+    author='Anton Burnashev',
+    author_email='fuss.here@gmail.com',
+    url='https://github.com/burnash/gspread',
+    keywords=['spreadsheets', 'google-spreadsheets'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    license='MIT'
+    )
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#imports
+# Imports
 import gspread
 from time import sleep
+from threading import Timer
 import os
 import sys
 import subprocess
@@ -9,11 +10,133 @@ from datetime import timedelta
 import urllib2
 import RPi.GPIO as GPIO
 
+############################################ Definitions ##############################################  
+streamURL = "http://2QMTL0.akacast.akamaistream.net/7/953/177387/v1/rc.akacast.akamaistream.net/2QMTL0"
+gDocURL = "1iO9__C7b31zWULhzjJbyP4z-LmNbKU_2CnkXlfL1QnA"
+gDocLogin = "george.koulouris1@gmail.com"
+gDocPSW = "CoolOuris=+"
+wakeVol = "80"
+sleepVol = "70"
+
+######################################### Global Variables ############################################## 
+connectionAttempts = 0
+isPlaying = False
+currSleepVol = wakeVol
+currWakeVol = sleepVol
+durarion = 30 
+wakeTime = 0
+sleepTime = 0
+
+# GPIO Pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.output(4, GPIO.LOW)
+
+############################################ Functions ############################################## 
+
+# Connection to Gdocs
+def gConnect():
+	
+	global gDocLogin, gDocPSW, wakeTime, sleepTime, duration	
+
+	# Login with your Google account
+	gc = gspread.Client(auth=(gDocLogin, gDocPSW))
+	
+	while True:	
+		try:
+			# Login & Connect to the spreadsheet
+			gc.login()
+			sht = gc.open_by_key(gDocURL)
+			worksheet = sht.get_worksheet(0)
+			
+			# Get the values from the cells
+			duration = int(worksheet.acell('B5').value)
+			wakeHour = int(worksheet.acell('B2').value)
+			wakeMin = int(worksheet.acell('C2').value)
+			sleepHour = int(worksheet.acell('B3').value)
+			sleepMin = int(worksheet.acell('C3').value)
+
+			# Combine the data to construct the wake & sleep times / + timedelta(minutes=duration)			
+			wakeTime = datetime.datetime(year=2014, month=11, day=15, hour=wakeHour, minute=wakeMin, second=0, microsecond=0)
+			sleepTime = datetime.datetime(year=2014, month=11, day=15, hour=sleepHour, minute=sleepMin, second=0, microsecond=0)
+
+			# Reset connection attempts counter
+			connectionAttempts = 0
+	
+			break
+		except:
+			print "Could not connect to GDocs.  Retrying..."
+
+			# Update the connection attempts variable
+			connectionAttempts = connectionAttempts + 1
+
+			# If too many failures, reboot
+			if connectionAttempts > 10:
+				os.system("sudo reboot")
+
+			# Reset in 10s
+			sleep(10)
+
+
+# Start playing music
+def startMusic(volume):
+	
+	global isPlaying
+
+	# Set-up the volume	
+	os.system("sudo mpc volume " + volume)
+	
+	# Start Playing
+	os.system("sudo mpc play 1")
+	isPlaying = True
+	
+	# Light-up the button
+	GPIO.output(4, GPIO.HIGH)
+
+# Stop playing music
+def stopMusic():
+	
+	global isPlaying
+	
+	os.system("sudo mpc pause")
+	isPlaying = False
+	
+	# Switch-off the button
+	GPIO.output(4, GPIO.LOW)
+
+# Button Press interrupt
+def buttonPress(pin):
+	global isPlaying, wakeVol, sleepVol
+
+	if not isPlaying:
+            	startMusic(str(wakeVol))		
+        else:
+            	stopMusic()
+
+######################################### Program Entry Point  ############################################## 
+
+# Set-up audio MPD
+os.system("sudo modprobe snd_bcm2835")
+os.system("sudo mpc clear")
+
+# Add the stream
+os.system("sudo mpc add " + streamURL)
+
+# Add an interrupt fot the button press
+GPIO.add_event_detect(17,GPIO.FALLING, callback=buttonPress, bouncetime=100)
+
+#### Main Loop ####
+while True:				
+
+	gConnect()
+
+'''
 # Check if internet is up
 def internet_on():
     try:
 	# http://74.125.228.100 is a server for google
-        response=urllib2.urlopen('http://74.125.228.100',timeout=1)
+        response=urllib2.urlopen('http://74.125.228.100')
         return True
     except urllib2.URLError as err: pass
     return False
@@ -36,55 +159,21 @@ eveningEnd = eveningStart + timedelta(minutes=delta)
 
 # GPIO Pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.output(18, GPIO.LOW)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.output(4, GPIO.LOW)
 
 ################### Google Docs Section ##############################
 
-def connectToDocs(): # Connect to docs & get values
-	print "Connecting to Google Docs"
-	
-	# Login with your Google account
-	gc = gspread.Client(auth=('', ''))
-	
-	while True:	
-		try:
-			gc.login()
-			sht = gc.open_by_key('1iO9__C7b31zWULhzjJbyP4z-LmNbKU_2CnkXlfL1QnA')
-			worksheet = sht.get_worksheet(0)
-	
-			# Set global variables
-			global delta, wakeHour, wakeMin, sleepHour, sleepMin, morningStart, eveningStart, morningEnd, eveningEnd
-			delta = int(worksheet.acell('B5').value)
-			wakeHour = int(worksheet.acell('B2').value)
-			wakeMin = int(worksheet.acell('C2').value)
-			sleepHour = int(worksheet.acell('B3').value)
-			sleepMin = int(worksheet.acell('C3').value)
-
-			morningStart = datetime.datetime(year=2014, month=11, day=15, hour=wakeHour, minute=wakeMin, second=0, microsecond=0)
-			eveningStart = datetime.datetime(year=2014, month=11, day=15, hour=sleepHour, minute=sleepMin, second=0, microsecond=0)
-			morningEnd = morningStart + timedelta(minutes=delta)
-			eveningEnd = eveningStart + timedelta(minutes=delta)	
-
-			# Print the results
-			print "Wake Up -> ",morningStart.time()," - ",morningEnd.time()
-			print "Sleep -> ",eveningStart.time()," - ",eveningEnd.time()
-			break
-		except:
-			print "Could not connect to GDocs.  Retrying..."
-			sleep(10)
 def buttonPress(pin):
 	global isPlaying
 
 	if not isPlaying:
-            	state = 1
-            	GPIO.output(18, GPIO.HIGH)
+            	GPIO.output(4, GPIO.HIGH)
 		os.system("sudo mpc play 1")
 		isPlaying = True		
         else:
-            	state = 0
-            	GPIO.output(18, GPIO.LOW)        
+            	GPIO.output(4, GPIO.LOW)        
 		os.system("sudo mpc pause")
 		isPlaying = False
 	 
@@ -97,7 +186,7 @@ os.system("sudo mpc volume 90")
 isPlaying = False
 
 # Add an interrupt fot the button press
-GPIO.add_event_detect(23,GPIO.FALLING, callback=buttonPress, bouncetime=100)
+GPIO.add_event_detect(17,GPIO.FALLING, callback=buttonPress, bouncetime=100)
 
 print "############################ Rasdio Playing! ####################"
 
@@ -112,20 +201,20 @@ while True:
 		if not isPlaying:
 			os.system("sudo mpc play 1")
 			isPlaying = True
-			GPIO.output(18, GPIO.HIGH)
+			GPIO.output(4, GPIO.HIGH)
 			print "Rise & shine!"
 
 	elif currentTime > eveningStart.time() and currentTime < eveningEnd.time():
         	if not isPlaying:
         	       	os.system("sudo mpc play 1")
 			isPlaying = True
-			GPIO.output(18, GPIO.HIGH)
+			GPIO.output(4, GPIO.HIGH)
 			print "Nighty nighty...!"
 
 	else:
 		if isPlaying:
 			os.system("sudo mpc pause")
-			#GPIO.output(18, GPIO.LOW)
+			#GPIO.output(4, GPIO.LOW)
 			isPlaying = False
-		
-		print "Check again...!"
+			print "Check again...!"
+'''
